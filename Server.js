22f6ca73e8ce77c52a6485f53258c46241532df2
feirie001@gmail.com <feirie001@gmail.com>
2022-01-27 15:24:44 +0100

    reset

--- Server.js
+++ Server.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const express = require("express");
+const res = require("express/lib/response");
+
+app = express();
+
+app.get("/", (req, res) => {
+    res.send("My App is up and running");
+})
+
+app.listen("/", (req, res) => {
+    console.log(`Server is running at port ${PORT}`);
+})
